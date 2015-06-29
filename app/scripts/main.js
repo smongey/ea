6f1cf1e4118f11d17ea4c07aa244fb9de5b0def1
smongey <sean@aokstudio.com>
2015-06-30 00:57:57 +0100

    scroll with magellan

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,11 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
-
-$(document).foundation();
+$(document).foundation({
+"magellan-expedition": {
+  active_class: 'active',
+  threshold: 0, // how many pixels until the magellan bar sticks, 0 = auto
+  destination_threshold: -5, // pixels from the top of destination for it to be considered active
+  throttle_delay: 50, // calculation throttling to increase framerate
+  fixed_top: 60, // top distance in pixels assigend to the fixed element on scroll
+  offset_by_height: true // whether to offset the destination by the expedition height. Usually you want this to be true, unless your expedition is on the side.
+}
+});
