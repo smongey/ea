7aed2397405f16a105f768e044a4e494faa733a4
smongey <sean@aokstudio.com>
2015-08-04 21:46:38 +0100

    updated nav, styled design section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,9 +3,9 @@ $(document).foundation({
 "magellan-expedition": {
   active_class: 'active',
   threshold: 0, // how many pixels until the magellan bar sticks, 0 = auto
-  destination_threshold: -5, // pixels from the top of destination for it to be considered active
+  destination_threshold: 10, // pixels from the top of destination for it to be considered active
   throttle_delay: 50, // calculation throttling to increase framerate
-  fixed_top: 60, // top distance in pixels assigend to the fixed element on scroll
+  fixed_top: 120, // top distance in pixels assigend to the fixed element on scroll
   offset_by_height: true // whether to offset the destination by the expedition height. Usually you want this to be true, unless your expedition is on the side.
 }
 });
@@ -18,7 +18,7 @@ var introHeight = $(window).height();
 
 $(window).on('scroll', function(e){
 	var current = e.currentTarget.pageYOffset;
-	if (current > introHeight) {
+	if (current > introHeight - 80) {
 		$('.contain-to-grid.fixed').addClass('active');
 	} else {
 		$('.contain-to-grid.fixed').removeClass('active');
